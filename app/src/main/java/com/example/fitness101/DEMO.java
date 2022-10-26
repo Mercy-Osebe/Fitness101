d4d5fab137e3d9d002c58ce5b3fea5351a61b3b4
engmercy <marcynyaata@gmail.com>
2022-10-26 17:58:14 +0300

    Started the android app,application splash screen working the activity main not launching

---
+++ app/src/main/java/com/example/fitness101/DEMO.java
@@ -0,0 +1,14 @@
+package com.example.fitness101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DEMO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
---
+++ app/src/main/java/com/example/fitness101/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.fitness101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+    private Animation up,down;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView imageView = findViewById(R.id.appsplash);
+        up=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.up);
+        imageView.setAnimation(up);
+
+
+        TextView textView = findViewById(R.id.appName);
+        down=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.down);
+        textView.setAnimation(down);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),DEMO.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3500);
+    }
+}
