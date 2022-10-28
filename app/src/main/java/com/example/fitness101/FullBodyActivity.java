7a19f9c74f8c89b365a1ba399ea25620c98c1136
engmercy <marcynyaata@gmail.com>
2022-10-28 14:28:54 +0300

    added the page for the highstepping_activity the card view has a bug

--- app/src/main/java/com/example/fitness101/FullBodyActivity.java
+++ app/src/main/java/com/example/fitness101/FullBodyActivity.java
@@ -2,13 +2,36 @@ package com.example.fitness101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class FullBodyActivity extends AppCompatActivity {
-
+    private static final String TAG = "FullBodyActivity";
+    private int[] ids;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_body);
+
+          ids=new int[]{
+                R.id.highstepping,R.id.sidehop,R.id.squats,R.id.wallpush_up,R.id.butt_bridge,R.id.firehydrantleft,R.id.firehydrantright,
+                R.id.cobrastretch,R.id.childpose,R.id.knee_to_chest_left,R.id.knee_to_chest_right,
+                R.id.crunches,R.id.planks,R.id.buttkicks};
+    }
+
+    public void imageClicked(View view) {
+        for (int i = 0; i < ids.length; i++) {
+            if(view.getId()==ids[i]){
+                int value=i+1;
+                Log.i(TAG, "imageClicked: first"+ value);
+                Intent intent=new Intent(getApplicationContext(),ShowFullBodyExcercisesActivity.class);
+                intent.putExtra("value",String.valueOf(value));
+                startActivity(intent);
+
+            }
+
+        }
     }
 }
