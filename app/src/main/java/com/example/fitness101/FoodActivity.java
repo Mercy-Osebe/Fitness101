f71390ee5102e7658302835675747e496c6d9473
Mercy Osebe <marcynyaata@gmail.com>
2022-11-08 14:39:07 +0300

    started the Food activity layouts

--- app/src/main/java/com/example/fitness101/FoodActivity.java
+++ app/src/main/java/com/example/fitness101/FoodActivity.java
@@ -1,14 +1,36 @@
 package com.example.fitness101;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
 
 public class FoodActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        listView=findViewById(R.id.listView);
+        
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
