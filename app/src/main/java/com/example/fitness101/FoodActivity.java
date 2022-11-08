bb53081e2a231c239ddf31fff882816cb02ab126
Mercy Osebe <marcynyaata@gmail.com>
2022-11-08 16:08:30 +0300

    added the food activity and the build activity tips

--- app/src/main/java/com/example/fitness101/FoodActivity.java
+++ app/src/main/java/com/example/fitness101/FoodActivity.java
@@ -4,12 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class FoodActivity extends AppCompatActivity {
     ListView listView;
+    private static final String TAG = "FoodActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,22 @@ public class FoodActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         listView=findViewById(R.id.listView);
-        
+        String [] titleStory=getResources().getStringArray(R.array.title_story);
+        String[] detailStory=getResources().getStringArray(R.array.details_story);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(getApplicationContext(),R.layout.list_row,R.id.row_text,titleStory);
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String details=detailStory[i];
+                Intent intent=new Intent(getApplicationContext(),BuildTipsActivity.class);
+                intent.putExtra("details",details);
+                startActivity(intent);
+                
+            }
+        });
+
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
