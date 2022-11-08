5dae1347cea05914781f85f48a80b7dff7bc6e11
Mercy Osebe <marcynyaata@gmail.com>
2022-11-08 10:52:24 +0300

    added the views

---
+++ app/src/main/java/com/example/fitness101/ThighWorkActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitness101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThighWorkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thigh_work);
+    }
+}
